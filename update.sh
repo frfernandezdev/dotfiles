e450a766af7cdc20cf9a81df971d8f663c8dbf96
Fernando Fernandez <frfernandezdev@gmail.com>
2021-08-10 11:59:20 -0400

    feat: diff local dotfiles with repo

--- update.sh
+++ update.sh
@@ -3,35 +3,35 @@
 if [[ -d ~/.config/nvim ]];then
 	echo "Check config nvim"
 
-	if diff nvim/init.vim ~/.config/nvim/init.vim;then
+	if ! diff -q ./nvim/init.vim ~/.config/nvim/init.vim &>/dev/null;then
 		cp ~/.config/nvim/init.vim ./nvim/init.vim
 	fi
-	if diff nvim/keys.vim ~/.config/nvim/keys.vim;then
+	if ! diff -q ./nvim/keys.vim ~/.config/nvim/keys.vim &>/dev/null;then
 		cp ~/.config/nvim/keys.vim ./nvim/keys.vim
 	fi
-	if diff nvim/line.vim ~/.config/nvim/line.vim;then
+	if ! diff -q ./nvim/line.vim ~/.config/nvim/line.vim &>/dev/null;then
 		cp ~/.config/nvim/line.vim ./nvim/line.vim
 	fi
-	if diff nvim/general.vim ~/.config/nvim/general.vim;then
+	if ! diff -q ./nvim/general.vim ~/.config/nvim/general.vim &>/dev/null;then
 		cp ~/.config/nvim/general.vim ./nvim/general.vim
 	fi
-	if diff nvim/plugins.vim ~/.config/nvim/plugins.vim;then
+	if ! diff -q ./nvim/plugins.vim ~/.config/nvim/plugins.vim &>/dev/null;then
 		cp ~/.config/nvim/plugins.vim ./nvim/plugins.vim
 	fi
-	if diff nvim/coc-settings.json ~/.config/nvim/coc-settings.json;then
+	if ! diff -q ./nvim/coc-settings.json ~/.config/nvim/coc-settings.json &>/dev/null;then
 		cp ~/.config/nvim/coc-settings.json ./nvim/coc-settings.json
 	fi
 	if [[ -d ~/.config/coc ]];then
-		if diff coc/memos.json ~/.config/coc/memos.json;then
+		if ! diff -q ./coc/memos.json ~/.config/coc/memos.json &>/dev/null;then
 			cp ~/.config/coc/memos.json ./coc/memos.json
 		fi
-		if diff coc/extensions/db.json ~/.config/coc/extensions/db.json;then
+		if ! diff -q ./coc/extensions/db.json ~/.config/coc/extensions/db.json &>/dev/null;then
 			cp ~/.config/coc/extensions/db.json ./coc/extensions/db.json
 		fi
-		if diff coc/extensions/package.json ~/.config/coc/extensions/package.json;then
+		if ! diff -q ./coc/extensions/package.json ~/.config/coc/extensions/package.json &>/dev/null;then
 			cp ~/.config/coc/extensions/package.json ./coc/extensions/package.json
 		fi
-		if diff coc/extensions/package-lock.json ~/.config/coc/extensions/package-lock.json;then
+		if ! diff -q ./coc/extensions/package-lock.json ~/.config/coc/extensions/package-lock.json &>/dev/null;then
 			cp ~/.config/coc/extensions/package-lock.json ./coc/extensions/package-lock.json
 		fi
 	fi
@@ -40,7 +40,7 @@ fi
 if [[ -d ~/.config/kitty ]];then
 	echo "Check config kitty"
 	
-	if diff kitty/kitty.conf ~/.config/kitty/kitty.conf;then
+	if ! diff -q ./kitty/kitty.conf ~/.config/kitty/kitty.conf &>/dev/null;then
 		cp ~/.config/kitty/kitty.conf ./kitty/kitty.conf
 	fi
 fi
